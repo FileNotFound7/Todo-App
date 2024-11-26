4986cfe75bf937ca6f17c541a6db2c318c4c25be
FileNotFound7 <rgharper@outlook.com.au>
2024-11-26 17:18:11 +1100

    add main.js and add task editor

---
+++ main.js
@@ -0,0 +1,13 @@
+class task {
+    constructor(id, name, description, priority, start, end){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.priority = priority;
+        this.start = start;
+        this.end = end;
+    }
+    edit() {
+
+    }
+}
