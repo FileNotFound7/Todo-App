895dab754d19bfbd50a0f683bd8b5264e0e6f394
FileNotFound7 <rgharper@outlook.com.au>
2025-02-14 09:49:41 +1100

    backend work

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,16 @@
 from flask import Flask, request
+from flask_cors import CORS
 import sqlite3, logging
 from sys import stderr
 
-expected_task_structure = ["taskid", "listid", "uid", "name", "content", "priority", "fromdate", "todate", "timestmamp"]
+expected_task_structure = ["taskid", "listid", "uid", "name", "content", "priority", "fromdate", "todate", "timestamp"]
 
 app = Flask(__name__)
+
+CORS(app)
+
+app.config["SERVER_NAME"] = "127.0.0.1:2999"
+app.config["DEBUG"] = True
 
 app.logger.addHandler(logging.FileHandler("app.log"))
 app.logger.addHandler(logging.StreamHandler(stderr))
@@ -12,24 +18,35 @@ app.logger.addHandler(logging.StreamHandler(stderr))
 db_conn = sqlite3.connect("main.sqlite3", check_same_thread=False)
 cur = db_conn.cursor()
 
-cur.execute("PRAGMA tableinfo(tasks)")
-task_structure = cur.fetchall()
-if task_structure != expected_task_structure:
-    app.logger.warning(f"tasks table is of {task_structure} instead of {expected_task_structure}")
+cur.execute("CREATE TABLE IF NOT EXISTS tasks (taskid, listid, uid, name, content, priority, fromdate, todate, timestamp)")
+# task_structure = cur.fetchall()
+# if task_structure != expected_task_structure:
+#     app.logger.warning(f"tasks table is of {task_structure} instead of {expected_task_structure}")
 
 @app.route("/tasklists/<string:uid>", methods=["GET"])
-def test(uid):
+def tasklists(uid):
     # headers = dict(request.headers)
     # headers[""]
     cur.execute("SELECT * FROM lists WHERE uid=?", (uid,))
     return cur.fetchall()
 
 @app.route("/task/<int:taskid>", methods=["GET", "POST"])
-def test(taskid):
+def task(taskid):
+    pass
     if request.method == "GET":
         cur.execute("SELECT * FROM tasks WHERE taskid=?", (taskid,))
         return cur.fetchall()
     else:
+        pass
         headers = dict(request.headers)
-        cur.execute("INSERT INTO tasks (name, content, priority, fromdate, todate) VALUES (?, ?, ?, ?, ?)", (headers["name"], headers["content"], headers["priority"], headers["fromdate"], headers["todate"]))
-        return 
+        cur.execute("UPDATE tasks SET ? VALUES ?", (headers["name"], headers["content"], headers["priority"], headers["fromdate"], headers["todate"]))
+        return
+    
+@app.route("/newtask", methods=["POST"])
+def newtask():
+    formData = request.form.to_dict()
+    cur.execute("INSERT INTO tasks (name, content, priority, fromdate, todate) VALUES (?, ?, ?, ?, ?)", (formData['name'], formData['description'], formData['priority'], formData['from'], formData['to']))
+    db_conn.commit()
+    return "OK", 200
+
+app.run()
--- frontend/main.js
+++ frontend/main.js
@@ -1,109 +1,114 @@
-class task {
+class taskEditor {
     constructor(parent, id, name = null, description = null, priority = null, start = null, end = null) {
         this.id = id;
-        this.name = name;
-        this.description = description;
-        this.priority = priority;
-        this.start = start;
-        this.end = end;
-        const priority_lv = ["1", "2", "3", "4", "5"];
+
+        var bg = document.createElement("form");
+        bg.className = 'task-bg'
 
         var form = document.createElement("form");
         form.id = `task_${id}`;
-        for (const level of priority_lv) {
+        form.className = 'task-editor'
+        this.formId = form.id
+
+        bg.appendChild(form)
+
+        document.body.appendChild(bg);
+
+        // make radio buttons
+        for (const level of ["1", "2", "3", "4", "5"]) {
             let radio = document.createElement("input");
             radio.type = "radio";
-            radio.name = `priority${level}`;
-            radio.class = `priority${level}`;
+            radio.name = 'priority';
+            radio.className = `priority${level}`;
             radio.value = level;
+            if (priority == level) {
+                radio.checked = true;
+            }
             form.appendChild(radio);
         }
 
+        // other elements
         var name = document.createElement("input");
-        name.id = "name";
+        name.name = "name";
         name.type = "text";
+        name.style = "textBox";
+        name.value = name;
         form.appendChild(name);
 
         var description = document.createElement("input");
-        description.id = "description";
+        description.name = "description"
         description.type = "text";
+        description.style = "textBox";
+        description.value = description;
         form.appendChild(description);
 
         var date_to = document.createElement("input");
-        date_to.id = "date_to";
+        date_to.name = 'to'
         date_to.type = "datetime-local";
+        name.style = "dateBox";
         form.appendChild(date_to);
 
         var date_from = document.createElement("input");
-        date_from.id = "date_from";
+        date_from.name = 'from'
         date_from.type = "datetime-local";
+        name.style = "dateBox";
         form.appendChild(date_from);
 
-        parent.appendChild(form);
-        form.addEventListener
+        const button = document.createElement("button")
+        button.textContent = "Submit!";
+        button.type = "submit"
+        button.value = "submit"
+        form.appendChild(button);
 
-        // let form = `<form id=${id}-row>
-        //     <input id="${id}-priority-1" type="radio" name="${id}-task-priority" class="priority1" value="1">
-        //     <input id="${id}-priority-2" type="radio" name="${id}-task-priority" class="priority2" value="2">
-        //     <input id="${id}-priority-3" type="radio" name="${id}-task-priority" class="priority3" value="3">
-        //     <input id="${id}-priority-4" type="radio" name="${id}-task-priority" class="priority4" value="4">
-        //     <input id="${id}-priority-5" type="radio" name="${id}-task-priority" class="priority5" value="5">
-        //     <input id="${id}-name" type="text">
-        //     <input id="${id}-description" type="text" style="width:100%; overflow:hidden;margin-right:10px;">
-        //     <input id="${id}-date-from" type="datetime-local">
-        //     <input id="${id}-date-to" type="datetime-local">
-        // </form>`
-
-        // parent.insertAdjacentHTML("beforeend", input);
-
-        if (id == "new") {
-            const newrow = document.getElementById("new-row");
-            const button = document.createElement("button")
-            button.id = "new-task";
-            button.textContent = "Create!";
-            button.type = "submit"
-            form.appendChild(button);
-        }
-
-        document.getElementById(`${this.id}-priority-1`).addEventListener("change", (event) => this.edit(event));
-        document.getElementById(`${this.id}-priority-2`).addEventListener("change", (event) => this.edit(event));
-        document.getElementById(`${this.id}-priority-3`).addEventListener("change", (event) => this.edit(event));
-        document.getElementById(`${this.id}-priority-4`).addEventListener("change", (event) => this.edit(event));
-        document.getElementById(`${this.id}-priority-5`).addEventListener("change", (event) => this.edit(event));
-        document.getElementById(`${this.id}-name`).addEventListener("change", (event) => this.edit(event));
-        document.getElementById(`${this.id}-description`).addEventListener("change", (event) => this.edit(event));
-        document.getElementById(`${this.id}-date-from`).addEventListener("change", (event) => this.edit(event));
-        document.getElementById(`${this.id}-date-to`).addEventListener("change", (event) => this.edit(event));
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            this.submit();
+        });
     }
-    edit(event) {
-        console.log(`${event.target.id} - ${event.target.value}`);
-        if (event.target.parentNode.parentNode.id != "new-row") {
+    // edit(event) {
+    //     console.log(`${event.target.id} - ${event.target.value}`);
+    //     if (event.target.parentNode.parentNode.id != "new-row") {
 
-        }
+    //     }
+    // }
+    // create(event) {
+    //     // const radio = document.querySelector(`input[name="${this.id}-task-priority"]:checked`);
+    //     // if (radio != null) {
+    //     //     this.priority = radio.value;
+    //     // } else {
+    //     //     this.priority = null;
+    //     // }
+    //     // this.name = document.getElementById(`${this.id}-name`).value;
+    //     // this.description = document.getElementById(`${this.id}-description`).value;
+    //     // this.start = document.getElementById(`${this.id}-date-from`).value;
+    //     // this.end = document.getElementById(`${this.id}-date-to`).value;
+
+
+
+    //     console.log(`${this.priority}, ${this.name}, ${this.description}, ${this.start}, ${this.end}`)
+
+    //     console.log("New task created.");
+    // }
+    async submit() {
+    // Associate the FormData object with the form element
+    const formData = new FormData(document.getElementById(this.formId));
+    formData.append(this.id)
+
+    const response = await fetch("http://127.0.0.1:2999/newtask", {
+        method: "POST",
+        // Set the FormData instance as the request body
+        body: formData,
+    });
+    console.log(await response.textContent);
     }
-    create(event) {
-        const radio = document.querySelector(`input[name="${this.id}-task-priority"]:checked`);
-        if (radio != null) {
-            this.priority = radio.value;
-        } else {
-            this.priority = null;
-        }
-        this.name = document.getElementById(`${this.id}-name`).value;
-        this.description = document.getElementById(`${this.id}-description`).value;
-        this.start = document.getElementById(`${this.id}-date-from`).value;
-        this.end = document.getElementById(`${this.id}-date-to`).value;
-
-        console.log(`${this.priority}, ${this.name}, ${this.description}, ${this.start}, ${this.end}`)
-
-        console.log("New task created.");
-    }
+    
 }
 
 function main() {
     let tasks = document.getElementById("tasks");
 
     console.log(tasks);
-    let input = new task(tasks, "new");
+    let input = new taskEditor(tasks, "new");
 }
 
 document.addEventListener("DOMContentLoaded", main)
